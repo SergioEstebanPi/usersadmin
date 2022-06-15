9c93e4c6b81058b020dec29fbf7d3410ccc68b28
SergioEstebanPi <sepi_147@hotmail.com>
2022-06-15 16:48:28 -0500

    Project initialization

---
+++ src/main/java/com/example/usersadmin/UsersadminApplication.java
@@ -0,0 +1,13 @@
+package com.example.usersadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersadminApplication.class, args);
+	}
+
+}
